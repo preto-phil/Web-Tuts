a6429be0fe785124cbdcc62dcb62877d01683dc1
preto-phil <104874252+preto-phil@users.noreply.github.com>
2023-12-01 14:42:25 +0200

    Snake Tut 1/2

---
+++ Games/Snake/snake.js
@@ -0,0 +1,22 @@
+export const SNAKE_SPEED = 1;
+
+const snakeBody = [{ x: 11, y: 11 }];
+
+export function update() {
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] }
+  }
+
+  snakeBody[0].x += 1;
+  snakeBody[0].y += 0;
+}
+
+export function draw(gameBoard) {
+  snakeBody.forEach((segment) => {
+    const snakeElement = document.createElement('div');
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
+    snakeElement.classList.add('snake');
+    gameBoard.appendChild(snakeElement);
+  })
+}
