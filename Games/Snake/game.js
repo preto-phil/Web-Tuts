72aa22f5a017265a279ba54266178959fd0eeb1c
preto-phil <104874252+preto-phil@users.noreply.github.com>
2024-03-01 23:43:28 +0200

    Snake tut fin

--- Games/Snake/game.js
+++ Games/Snake/game.js
@@ -1,15 +1,23 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake.js";
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
 
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
+  if (gameOver) {
+    if (confirm("You lost. Press ok to restart.")) {
+      window.location = '/';
+    }
+    return;
+  }
   window.requestAnimationFrame(main);
   const secondSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondSinceLastRender < 1 / SNAKE_SPEED) return;
   
   lastRenderTime = currentTime;
-  console.log('Render');
   
   update();
   draw();
@@ -18,10 +26,18 @@ function main(currentTime) {
 window.requestAnimationFrame(main);
 
 function update() {
-  updateSnake()
+  updateSnake();
+  updateFood();
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = '';
   drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
